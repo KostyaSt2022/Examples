e357c6dd421b38d241544b9ebf72e639b8483e36
Kostya <kotekostya@gmail.com>
2022-10-14 18:26:15 +0400

    add change example004_Div

--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 17;
+double numberB = 8;
+Console.WriteLine(numberA / numberB);
